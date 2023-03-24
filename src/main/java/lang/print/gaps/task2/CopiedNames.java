7472d73deb8903550f967efe43aa112e558d779d
Meerim Sadykbekova <sadykbekova.m@gmail.com>
2023-03-24 18:07:43 +0600

    Renamed vars in CopiedNames file.

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable_1 = 0;
-        int variable_2 = 2000;
-        int value_1 = 1000;
-        int variable_3 = 4000;
-        int value_2 = 3000;
-        int variable_4 = 5000;
-        int variable_5 = 7000;
+        int variable1 = 0;
+        int variable2 = 2000;
+        int value1 = 1000;
+        int variable3 = 4000;
+        int value2 = 3000;
+        int variable4 = 5000;
+        int variable5 = 7000;
         int value = 90000;
     }
 }
